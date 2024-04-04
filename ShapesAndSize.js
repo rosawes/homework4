da0896e03baaa724005d240f5d8b6c59fb40da05
rosawes <157071862+rosawes@users.noreply.github.com>
2024-04-03 23:06:23 -0400

    Add files via upload

--- ShapesAndSize.js
+++ ShapesAndSize.js
@@ -1,21 +1,36 @@
 import React from 'react';
 
 export const svgShapes = {
-    0: <circle fill="red" r="30" cx="50%" cy="50%" />,
-    1: <circle fill="blue" r="30" cx="50%" cy="50%" />,
-    2: <circle fill="green" r="30" cx="50%" cy="50%" />,
-    3: <rect fill="red" height="60" width="60" rx="5" x="20" y="45" />,
-    4: <rect fill="blue" height="60" width="60" rx="5" x="20" y="45" />,
-    5: <rect fill="green" height="60" width="60" rx="5" x="20" y="45" />,
-    6: <polygon fill="orange" points="50,15 80,80 20,80" />,
-    7: <polygon fill="black" points="50,10 90,50 50,90 10,50" />, 
-    8: <polygon fill="red" points="50,15 80,80 20,80" />,
-    9: <polygon fill="green" points="50,15 80,80 20,80" />,
-    10: <polygon fill="blue" points="50,10 90,50 50,90 10,50" />,
-    11: <circle fill="orange" r="30" cx="50%" cy="50%" />,
-    12: <circle fill="black" r="30" cx="50%" cy="50%" />,
-    13: <rect fill="orange" height="60" width="60" rx="5" x="20" y="20" />,
-    14: <rect fill="black" height="60" width="60" rx="5" x="20" y="20" />,
+    0: <svg viewBox="0 0 100 100">
+        <circle fill="red" r="30" cx="50%" cy="50%" /></svg>,
+    1: <svg viewBox="0 0 100 100">
+        <circle fill="blue" r="30" cx="50%" cy="50%" /></svg>,
+    2: <svg viewBox="0 0 100 100">
+        <circle fill="green" r="30" cx="50%" cy="50%" /></svg>,
+    3: <svg viewBox="0 0 100 100">
+        <rect fill="red" height="60" width="60" rx="5" x="20" y="20" /></svg>,
+    4: <svg viewBox="0 0 100 100">
+        <rect fill="blue" height="60" width="60" rx="5" x="20" y="20" /></svg>,
+    5: <svg viewBox="0 0 100 100">
+        <rect fill="green" height="60" width="60" rx="5" x="20" y="20" /></svg>,
+    6: <svg viewBox="0 0 100 100">
+        <polygon fill="orange" points="50,15 80,80 20,80" /></svg>,
+    7: <svg viewBox="0 0 100 100">
+        <polygon fill="black" points="50,10 90,50 50,90 10,50" /></svg>, 
+    8: <svg viewBox="0 0 100 100">
+        <polygon fill="red" points="50,15 80,80 20,80" /></svg>,
+    9: <svg viewBox="0 0 100 100">
+        <polygon fill="green" points="50,15 80,80 20,80" /></svg>,
+    10: <svg viewBox="0 0 100 100">
+        <polygon fill="blue" points="50,10 90,50 50,90 10,50" /></svg>,
+    11: <svg viewBox="0 0 100 100">
+        <circle fill="orange" r="30" cx="50%" cy="50%" /></svg>,
+    12: <svg viewBox="0 0 100 100">
+        <circle fill="black" r="30" cx="50%" cy="50%" /></svg>,
+    13: <svg viewBox="0 0 100 100">
+        <rect fill="orange" height="60" width="60" rx="5" x="20" y="20" /></svg>,
+    14: <svg viewBox="0 0 100 100">
+        <rect fill="black" height="60" width="60" rx="5" x="20" y="20" /></svg>,
 };
 
 export const boardSizes = {
